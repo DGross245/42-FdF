d8dd7199c6e72019cad1e40176029cedc3a30f89
DGross245 <103501922+DGross245@users.noreply.github.com>
2022-09-20 17:52:15 +0200

    inprogress

---
+++ src/main.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/29 19:00:50 by dgross            #+#    #+#             */
+/*   Updated: 2022/09/20 15:39:12 by dgross           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MLX42.h"
+#include <fcntl.h>
+#include "libft.h"
+#include "get_next_line.h"
+#include <stdio.h>
+#include "fdf.h"
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	t_fdf	fdf;
+	t_map	map;
+
+	map.stack = NULL;
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 1)
+			return (0); // MUSS EIN ERROR SEIN
+		fdf.mlx = mlx_init(WIDTH, HEIGHT, "FdF", true);
+		if (fdf.mlx == NULL)
+			return (0); // MUSS EIN ERROR SEIN
+		read_map(fd, &map);
+		//create_map();
+		//fdf.img = mlx_new_image(fdf.mlx, 128, 128);
+		//memset(fdf.img->pixels, 255, fdf.img->width * fdf.img->height * sizeof(int));
+		//mlx_image_to_window(fdf.mlx, fdf.img, 0, 0);
+		//mlx_loop_hook(fdf.mlx, &hook, &fdf);
+		//mlx_loop(fdf.mlx);
+		//mlx_terminate(fdf.mlx);
+	}
+	return (0);
+}
