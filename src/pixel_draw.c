361f695e6e8282913a8fd60a9f89e495d3fbb319
DGross245 <103501922+DGross245@users.noreply.github.com>
2022-09-28 20:48:44 +0200

    s

--- src/pixel_draw.c
+++ src/pixel_draw.c
@@ -6,7 +6,7 @@
 /*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 15:26:17 by dgross            #+#    #+#             */
-/*   Updated: 2022/09/28 01:57:04 by dgross           ###   ########.fr       */
+/*   Updated: 2022/09/28 20:43:16 by dgross           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,12 +45,10 @@ void	create_map(t_map *map, t_fdf *fdf)
 
 t_point	make_point(int x, int y, t_map *map)
 {
-	t_point	coords;
-
-	coords.x = x;
-	coords.y = y;
-	coords.z = map->str[map->width * y + x];
-	return (coords);
+	map->stack->point.x = x;
+	map->stack->point.y = y;
+	map->stack->point.z = map->str[map->width * y + x];
+	return (map->stack->point);
 }
 
 void	draw_line(t_point start, t_point end, t_fdf *fdf)
@@ -76,25 +74,47 @@ void	draw_line(t_point start, t_point end, t_fdf *fdf)
 	}
 }
 
-void	iso(float *x, float *y, float z)
+void	iso(float *x, float *y, float *z)
 {
-	float	previous_x;
-	float	previous_y;
+	float	x_tmp;
+	float	y_tmp;
 
-	previous_x = *x;
-	previous_y = *y;
-	*x = (previous_x - previous_y) * cos(0.523599);
-	*y = -z + (previous_x + previous_y) * sin(0.523599);
+	x_tmp = *x;
+	y_tmp = *y;
+	*x = (x_tmp - y_tmp) * cos(0.523599);
+	*y = -*z + (x_tmp + y_tmp) * sin(0.523599);
+}
+
+void	rotate_x(float *y, float *z, float alpha)
+{
+	float	y_tmp;
+
+	y_tmp = *y;
+	*y = y_tmp * cos(alpha) + *z * sin(alpha);
+	*z = -y_tmp * sin(alpha) + *z * cos(alpha);
+}
+
+void	rotate_y(float *x, float *z, float beta)
+{
+	float	x_tmp;
+
+	x_tmp = *x;
+	*x = x_tmp * cos(beta) + *x * sin(beta);
+	*z = -x_tmp * sin(beta) + *x * cos(beta);
 }
 
 t_point	calc_coord(t_map *map, t_point coords)
 {
 	coords.x *= map->cam.zoom;
 	coords.y *= map->cam.zoom;
-	coords.x -= (map->cam.zoom * map->width) / 2;
-	coords.y -= (map->cam.zoom * map->height) / 2;
-	iso(&coords.x, &coords.y, coords.z);
-	coords.x += WIDTH / 2;
-	coords.y += (HEIGHT + map->cam.zoom * map->height) / 2;
+	coords.z *= map->cam.zoom / 2 + coords.z_incr;
+	coords.x -= ((map->cam.zoom * map->width) / 2);
+	coords.y -= ((map->cam.zoom * map->height) / 2);
+	rotate_x(&coords.x, &coords.x, map->stack->point.alpha);
+	rotate_y(&coords.x, &coords.x, map->stack->point.beta);
+	if (map->cam.projection == 1)
+		iso(&coords.x, &coords.y, &coords.z);
+	coords.x += (WIDTH / 2) + coords.x_offset;
+	coords.y += (HEIGHT / 2) + coords.y_offset;
 	return (coords);
 }
